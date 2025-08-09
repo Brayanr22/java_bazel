4a4157818c8332991cf40736db606785d9c36b38
Brayanr22 <brnrolando98@gmail.com>
2025-08-08 20:04:20 -0500

    paso 6

---
+++ app1/src/main/java/com/ejemplo/app1/Main.java
@@ -0,0 +1,11 @@
+package main.java.com.ejemplo.app1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Hola desde Bazel!");
+        
+        Calculadora calc = new Calculadora();
+        int resultado = calc.sumar(5, 3);
+        System.out.println("5 + 3 = " + resultado);
+    }
+}
